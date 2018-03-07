b686d867453e4375b4687d812f14dd0db4a15e8e
Andrii.Hrynkiv <Andrii.Hrynkiv@eleks.com>
2018-03-07 18:41:06 +0200

    added list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,27 @@ import { cryptocurrencyServices } from '../services/cryptocurrency.service'
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  cryptocurrency: Array<any>;
-  constructor(private _cryptocurrencyServices : cryptocurrencyServices ) { }
+  cryptocurrency: Array<object>;
+  viewItems: Array<object>;
+  constructor(private _cryptocurrencyServices: cryptocurrencyServices) { }
 
   ngOnInit(): void {
     this._cryptocurrencyServices.getCryptocurrencies()
-        .subscribe(data => this.cryptocurrency = data);  
+      .subscribe(data => {
+        this.cryptocurrency = data;
+        this.viewItems = this.cryptocurrency.slice(0, 12);
+      });
+
   }
+  
+  onAddMoreItems(): void {
+    let currentItemsArray: Array<object> = this.viewItems;
+    let lastIndex: number = currentItemsArray.length;
+    let newItemsArray = this.cryptocurrency.slice(lastIndex, lastIndex+8);
+    this.viewItems = currentItemsArray.concat(newItemsArray);
+  }
+
 }
+
+
+
