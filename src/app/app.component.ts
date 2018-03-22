7388fe59a159ec79333d991e6df28542d0846d46
Andrii.Hrynkiv <Andrii.Hrynkiv@eleks.com>
2018-03-22 15:02:46 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { cryptocurrencyServices } from './services/cryptocurrency.service';
+import { MyCurrencyListService } from './services/my-currency-list.service';
+import { AuthService } from './services/auth.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  providers: [cryptocurrencyServices, MyCurrencyListService]
 })
 export class AppComponent {
+
+  
   ngOnInit(): void {}
 
 }
